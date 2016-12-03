adce3ba646c134193ce577d51bee1fb5f0e5dad0
Andy Goldstein <agoldste@redhat.com>
2016-12-03 06:35:24 -0500

    pkg/cmd/admin/registry: match upstream refactors

--- pkg/cmd/admin/registry/registry.go
+++ pkg/cmd/admin/registry/registry.go
@@ -242,7 +242,7 @@ func (opts *RegistryOptions) Complete(f *clientcmd.Factory, cmd *cobra.Command, 
 		return fmt.Errorf("error getting namespace: %v", nsErr)
 	}
 
-	_, _, kClient, kClientErr := f.Clients()
+	_, kClient, kClientErr := f.Clients()
 	if kClientErr != nil {
 		return fmt.Errorf("error getting client: %v", kClientErr)
 	}
@@ -467,7 +467,7 @@ func (opts *RegistryOptions) RunCmdRegistry() error {
 	list := &kapi.List{Items: objects}
 
 	if opts.Config.Action.ShouldPrint() {
-		mapper, _ := opts.factory.Object(false)
+		mapper, _ := opts.factory.Object()
 		fn := cmdutil.VersionedPrintObject(opts.factory.PrintObject, opts.cmd, mapper, opts.out)
 		if err := fn(list); err != nil {
 			return fmt.Errorf("unable to print object: %v", err)
