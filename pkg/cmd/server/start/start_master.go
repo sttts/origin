993c64b0b5528a8ca715051d5000da1bd6375a3f
Andy Goldstein <agoldste@redhat.com>
2016-12-09 10:56:35 -0500

    Make jobClient use batch/v2alpha1 to support cronjobs

--- pkg/cmd/server/start/start_master.go
+++ pkg/cmd/server/start/start_master.go
@@ -17,6 +17,7 @@ import (
 
 	cmapp "k8s.io/kubernetes/cmd/kube-controller-manager/app/options"
 	kerrors "k8s.io/kubernetes/pkg/api/errors"
+	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/apimachinery/registered"
 	"k8s.io/kubernetes/pkg/apis/apps"
 	"k8s.io/kubernetes/pkg/apis/autoscaling"
@@ -598,10 +599,15 @@ func startControllers(oc *origin.MasterConfig, kc *kubernetes.MasterConfig) erro
 		if err != nil {
 			glog.Fatalf("Could not get client for deployment controller: %v", err)
 		}
+
+		// TODO there has to be a better way to do this
+		oc.PrivilegedLoopbackClientConfig.ContentConfig.GroupVersion = &unversioned.GroupVersion{Group: "batch", Version: "v2alpha1"}
 		_, _, jobClient, err := oc.GetServiceAccountClients(bootstrappolicy.InfraJobControllerServiceAccountName)
 		if err != nil {
 			glog.Fatalf("Could not get client for job controller: %v", err)
 		}
+		oc.PrivilegedLoopbackClientConfig.ContentConfig.GroupVersion = nil
+
 		_, hpaOClient, hpaKClient, err := oc.GetServiceAccountClients(bootstrappolicy.InfraHPAControllerServiceAccountName)
 		if err != nil {
 			glog.Fatalf("Could not get client for HPA controller: %v", err)
