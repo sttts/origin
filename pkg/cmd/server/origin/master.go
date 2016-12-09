e2d0f8afd8d9f912571a36b779388be9d3f81c9b
Andy Goldstein <agoldste@redhat.com>
2016-12-09 11:24:52 -0500

    Use kube's /healthz handler
    Squashed commit of the following:
    
    commit 8c16907b9619f5f465dc2b5e49d24ee963fdd30b
    Author: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
    Date:   Fri Dec 9 15:21:12 2016 +0100
    
        pkg/cmd/server/origin/master.go: use upstream /healthz
    
    commit 0bbc4862960ce472fc949c002600fcdeef70cbc9
    Author: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
    Date:   Fri Dec 9 15:19:54 2016 +0100
    
        pkg/cmd/server/origin/master.go: comment that we override /healthz

--- pkg/cmd/server/origin/master.go
+++ pkg/cmd/server/origin/master.go
@@ -517,7 +517,6 @@ func (c *MasterConfig) InstallProtectedAPI(container *restful.Container) ([]stri
 	initAPIVersionRoute(root, OpenShiftAPIPrefix, currentAPIVersions...)
 
 	initControllerRoutes(root, "/controllers", c.Options.Controllers != configapi.ControllersDisabled, c.ControllerPlug)
-	initHealthCheckRoute(root, "/healthz")
 	initReadinessCheckRoute(root, "/healthz/ready", c.ProjectAuthorizationCache.ReadyForAccess)
 	initVersionRoute(container, "/version/openshift")
 
