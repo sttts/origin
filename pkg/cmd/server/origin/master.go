0bbc4862960ce472fc949c002600fcdeef70cbc9
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2016-12-09 15:19:54 +0100

    pkg/cmd/server/origin/master.go: comment that we override /healthz

--- pkg/cmd/server/origin/master.go
+++ pkg/cmd/server/origin/master.go
@@ -517,6 +517,8 @@ func (c *MasterConfig) InstallProtectedAPI(container *restful.Container) ([]stri
 	initAPIVersionRoute(root, OpenShiftAPIPrefix, currentAPIVersions...)
 
 	initControllerRoutes(root, "/controllers", c.Options.Controllers != configapi.ControllersDisabled, c.ControllerPlug)
+	// this overrides genericapiserver /healthz
+	// TODO(sttts): switch to upstream healthz handler
 	initHealthCheckRoute(root, "/healthz")
 	initReadinessCheckRoute(root, "/healthz/ready", c.ProjectAuthorizationCache.ReadyForAccess)
 	initVersionRoute(container, "/version/openshift")
