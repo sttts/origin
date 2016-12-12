b5849535d5bb8844cfb3c53f32e85bd248ef58b9
Andy Goldstein <agoldste@redhat.com>
2016-12-12 11:08:30 -0500

    test/extended/networking: make it compile

--- test/extended/networking/ovs.go
+++ test/extended/networking/ovs.go
@@ -28,7 +28,7 @@ var _ = Describe("[networking] OVS", func() {
 		oc := testexutil.NewCLI("get-flows", testexutil.KubeConfigPath())
 
 		It("should add and remove flows when pods are added and removed", func() {
-			nodes := e2e.GetReadySchedulableNodesOrDie(f1.Client)
+			nodes := e2e.GetReadySchedulableNodesOrDie(f1.ClientSet)
 			origFlows := getFlowsForAllNodes(oc, nodes.Items)
 			Expect(len(origFlows)).To(Equal(len(nodes.Items)))
 			for _, flows := range origFlows {
@@ -82,7 +82,7 @@ var _ = Describe("[networking] OVS", func() {
 			}
 			Expect(reflect.DeepEqual(origFlows[deployNodeName], otherFlows)).To(BeTrue(), "Flows on deployed-to node should be unchanged except for the new pod")
 
-			err := f1.Client.Pods(f1.Namespace.Name).Delete(podName, nil)
+			err := f1.ClientSet.Core().Pods(f1.Namespace.Name).Delete(podName, &kapi.DeleteOptions{})
 			Expect(err).NotTo(HaveOccurred())
 
 			postDeleteFlows := getFlowsForNode(oc, deployNodeName)
@@ -91,7 +91,7 @@ var _ = Describe("[networking] OVS", func() {
 
 		It("should add and remove flows when nodes are added and removed", func() {
 			var err error
-			nodes := e2e.GetReadySchedulableNodesOrDie(f1.Client)
+			nodes := e2e.GetReadySchedulableNodesOrDie(f1.ClientSet)
 			origFlows := getFlowsForAllNodes(oc, nodes.Items)
 
 			// The SDN/OVS code doesn't care that the node doesn't actually exist,
@@ -132,9 +132,9 @@ var _ = Describe("[networking] OVS", func() {
 					},
 				},
 			}
-			node, err = f1.Client.Nodes().Create(node)
+			node, err = f1.ClientSet.Core().Nodes().Create(node)
 			Expect(err).NotTo(HaveOccurred())
-			defer f1.Client.Nodes().Delete(node.Name)
+			defer f1.ClientSet.Core().Nodes().Delete(node.Name, &kapi.DeleteOptions{})
 
 			osClient, err := testutil.GetClusterAdminClient(testexutil.KubeConfigPath())
 			Expect(err).NotTo(HaveOccurred())
@@ -175,7 +175,7 @@ var _ = Describe("[networking] OVS", func() {
 				Expect(reflect.DeepEqual(origFlows[nodeName], otherFlows)).To(BeTrue(), "Flows should be unchanged except for the new node")
 			}
 
-			err = f1.Client.Nodes().Delete(node.Name)
+			err = f1.ClientSet.Core().Nodes().Delete(node.Name, &kapi.DeleteOptions{})
 			Expect(err).NotTo(HaveOccurred())
 			e2e.Logf("Waiting up to %v for HostSubnet to be deleted", hostSubnetTimeout)
 			for start := time.Now(); time.Since(start) < hostSubnetTimeout; time.Sleep(time.Second) {
@@ -196,7 +196,7 @@ var _ = Describe("[networking] OVS", func() {
 		oc := testexutil.NewCLI("get-flows", testexutil.KubeConfigPath())
 
 		It("should add and remove flows when services are added and removed", func() {
-			nodes := e2e.GetReadySchedulableNodesOrDie(f1.Client)
+			nodes := e2e.GetReadySchedulableNodesOrDie(f1.ClientSet)
 			origFlows := getFlowsForAllNodes(oc, nodes.Items)
 
 			serviceName := "ovs-test-service"
@@ -216,9 +216,9 @@ var _ = Describe("[networking] OVS", func() {
 				Expect(foundServiceFlow).To(BeTrue(), "Each node contains a rule for the service")
 			}
 
-			err := f1.Client.Pods(f1.Namespace.Name).Delete(serviceName, nil)
+			err := f1.ClientSet.Core().Pods(f1.Namespace.Name).Delete(serviceName, &kapi.DeleteOptions{})
 			Expect(err).NotTo(HaveOccurred())
-			err = f1.Client.Services(f1.Namespace.Name).Delete(serviceName)
+			err = f1.ClientSet.Core().Services(f1.Namespace.Name).Delete(serviceName, &kapi.DeleteOptions{})
 			Expect(err).NotTo(HaveOccurred())
 
 			postDeleteFlows := getFlowsForAllNodes(oc, nodes.Items)
@@ -269,13 +269,13 @@ func doGetFlowsForNode(oc *testexutil.CLI, nodeName string) ([]string, error) {
 		},
 	}
 	f := oc.KubeFramework()
-	podClient := f.Client.Pods(f.Namespace.Name)
+	podClient := f.ClientSet.Core().Pods(f.Namespace.Name)
 	pod, err := podClient.Create(pod)
 	if err != nil {
 		return nil, err
 	}
-	defer podClient.Delete(pod.Name, nil)
-	err = waitForPodSuccessInNamespace(f.Client, pod.Name, "flow-check", f.Namespace.Name)
+	defer podClient.Delete(pod.Name, &kapi.DeleteOptions{})
+	err = waitForPodSuccessInNamespace(f.ClientSet, pod.Name, "flow-check", f.Namespace.Name)
 	if err != nil {
 		return nil, err
 	}
