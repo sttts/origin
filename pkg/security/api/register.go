f2df52a40917c8fdcbd3bb8665dc480d2e05bfc0
Michal Fojtik <mfojtik@redhat.com>
2017-02-22 14:51:58 +0100

    api: register origin types into API groups
    * Deprecate the current scheme in LegacyScheme
    * Install new API group using announced.NewGroupMetaFactory()

--- pkg/security/api/register.go
+++ pkg/security/api/register.go
@@ -1,17 +1,28 @@
 package api
 
 import (
+	kapi "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/runtime"
+	"k8s.io/kubernetes/pkg/watch/versioned"
 )
 
-const GroupName = ""
-
-// TODO no one likes the name security because it so broad as to be meaningless.
-// const FutureGroupName = "security.openshift.io"
+const (
+	GroupName       = "security.openshift.io"
+	LegacyGroupName = ""
+)
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = unversioned.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
+var (
+	SchemeGroupVersion       = unversioned.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
+	LegacySchemeGroupVersion = unversioned.GroupVersion{Group: LegacyGroupName, Version: runtime.APIVersionInternal}
+
+	LegacySchemeBuilder    = runtime.NewSchemeBuilder(addLegacyKnownTypes)
+	AddToSchemeInCoreGroup = LegacySchemeBuilder.AddToScheme
+
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) unversioned.GroupKind {
@@ -23,25 +34,31 @@ func Resource(resource string) unversioned.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
-var (
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	AddToScheme   = SchemeBuilder.AddToScheme
-)
-
 // Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
+	types := []runtime.Object{
 		&PodSecurityPolicySubjectReview{},
 		&PodSecurityPolicySelfSubjectReview{},
 		&PodSecurityPolicyReview{},
+	}
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		append(types,
+			&unversioned.Status{}, // TODO: revisit in 1.6 when Status is actually registered as unversioned
+			&kapi.ListOptions{},
+			&kapi.DeleteOptions{},
+			&kapi.ExportOptions{},
+		)...,
 	)
+	versioned.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
 
-func (obj *PodSecurityPolicySubjectReview) GetObjectKind() unversioned.ObjectKind {
-	return &obj.TypeMeta
+func addLegacyKnownTypes(scheme *runtime.Scheme) error {
+	types := []runtime.Object{
+		&PodSecurityPolicySubjectReview{},
+		&PodSecurityPolicySelfSubjectReview{},
+		&PodSecurityPolicyReview{},
+	}
+	scheme.AddKnownTypes(LegacySchemeGroupVersion, types...)
+	return nil
 }
-func (obj *PodSecurityPolicySelfSubjectReview) GetObjectKind() unversioned.ObjectKind {
-	return &obj.TypeMeta
-}
-func (obj *PodSecurityPolicyReview) GetObjectKind() unversioned.ObjectKind { return &obj.TypeMeta }
