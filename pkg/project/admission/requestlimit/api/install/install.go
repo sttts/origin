65be82f1defc9a114c72219283cb04531e15bf7f
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-02 11:18:02 +0100

    admission/requestlimit/api: install into configapi.Scheme

--- pkg/project/admission/requestlimit/api/install/install.go
+++ pkg/project/admission/requestlimit/api/install/install.go
@@ -16,7 +16,7 @@ const importPrefix = "github.com/openshift/origin/pkg/project/admission/requestl
 var accessor = meta.NewAccessor()
 
 // availableVersions lists all known external versions for this group from most preferred to least preferred
-var availableVersions = []unversioned.GroupVersion{v1.LegacySchemeGroupVersion}
+var availableVersions = []unversioned.GroupVersion{v1.SchemeGroupVersion, v1.LegacySchemeGroupVersion}
 
 func init() {
 	if err := enableVersions(availableVersions); err != nil {
@@ -35,10 +35,13 @@ func enableVersions(externalVersions []unversioned.GroupVersion) error {
 
 func addVersionsToScheme(externalVersions ...unversioned.GroupVersion) {
 	// add the internal version to Scheme
+	api.AddToScheme(configapi.Scheme)
 	api.AddToSchemeInCoreGroup(configapi.Scheme)
 	// add the enabled external versions to Scheme
 	for _, v := range externalVersions {
 		switch v {
+		case v1.SchemeGroupVersion:
+			v1.AddToScheme(configapi.Scheme)
 		case v1.LegacySchemeGroupVersion:
 			v1.AddToSchemeInCoreGroup(configapi.Scheme)
 
