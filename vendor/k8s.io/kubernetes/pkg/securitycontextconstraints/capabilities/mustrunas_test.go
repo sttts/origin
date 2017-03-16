8bee2855080dbac2fbba8bbe4c8399f38816398a
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-16 14:45:43 +0100

    REBASE: bump(*): latest Godeps.json sha1s + some fake dir commits

--- vendor/k8s.io/kubernetes/pkg/securitycontextconstraints/capabilities/mustrunas_test.go
+++ vendor/k8s.io/kubernetes/pkg/securitycontextconstraints/capabilities/mustrunas_test.go
@@ -258,13 +258,6 @@ func TestValidateAdds(t *testing.T) {
 			},
 			shouldPass: true,
 		},
-		"no required, all allowed, container requests valid": {
-			allowedCaps: []api.Capability{api.CapabilityAll},
-			containerCaps: &api.Capabilities{
-				Add: []api.Capability{"foo"},
-			},
-			shouldPass: true,
-		},
 		"no required, allowed, container requests invalid": {
 			allowedCaps: []api.Capability{"foo"},
 			containerCaps: &api.Capabilities{
