4bd1ff25f0b47361b308932e5dfde8ec5fb771b6
David Eads <deads@redhat.com>
2017-12-08 14:38:22 -0500

    DANW

--- pkg/cmd/server/kubernetes/network/sdn_unsupported.go
+++ pkg/cmd/server/kubernetes/network/sdn_unsupported.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	kclientset "k8s.io/client-go/kubernetes"
-	"k8s.io/kubernetes/pkg/apis/componentconfig"
+	"k8s.io/kubernetes/pkg/apis/kubeproxyconfig"
 	kinternalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	kinternalinformers "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalversion"
 
@@ -15,6 +15,6 @@ import (
 	networkclient "github.com/openshift/origin/pkg/network/generated/internalclientset"
 )
 
-func NewSDNInterfaces(options configapi.NodeConfig, networkClient networkclient.Interface, kubeClientset kclientset.Interface, kubeClient kinternalclientset.Interface, internalKubeInformers kinternalinformers.SharedInformerFactory, proxyconfig *componentconfig.KubeProxyConfiguration) (network.NodeInterface, network.ProxyInterface, error) {
+func NewSDNInterfaces(options configapi.NodeConfig, networkClient networkclient.Interface, kubeClientset kclientset.Interface, kubeClient kinternalclientset.Interface, internalKubeInformers kinternalinformers.SharedInformerFactory, proxyconfig *kubeproxyconfig.KubeProxyConfiguration) (network.NodeInterface, network.ProxyInterface, error) {
 	return nil, nil, fmt.Errorf("SDN not supported on this platform")
 }
