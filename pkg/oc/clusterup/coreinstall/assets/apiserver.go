cf9b712b604f80cffe8715a733ccd8254495f4fe
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-09-25 10:06:38 +0200

    cluster-up: split ca into kube-ca and root-ca, like kube-core

--- pkg/oc/clusterup/coreinstall/assets/apiserver.go
+++ pkg/oc/clusterup/coreinstall/assets/apiserver.go
@@ -9,8 +9,10 @@ import (
 )
 
 const (
-	AssetPathCAKey                 = "tls/ca.key"
-	AssetPathCACert                = "tls/ca.crt"
+	AssetPathRootCAKey             = "tls/root-ca.key"
+	AssetPathRootCACert            = "tls/root-ca.crt"
+	AssetPathKubeCAKey             = "tls/kube-ca.key"
+	AssetPathKubeCACert            = "tls/kube-ca.crt"
 	AssetPathAPIServerKey          = "tls/apiserver.key"
 	AssetPathAPIServerCert         = "tls/apiserver.crt"
 	AssetPathServiceAccountPrivKey = "tls/service-account.key"
@@ -73,8 +75,10 @@ func (r *TLSAssetsRenderOptions) newTLSAssets(caCert *x509.Certificate, caPrivKe
 	r.config.AdminCert = tlsutil.EncodeCertificatePEM(adminCert)
 
 	assets = append(assets, []assetslib.Asset{
-		{Name: AssetPathCAKey, Data: tlsutil.EncodePrivateKeyPEM(caPrivKey)},
-		{Name: AssetPathCACert, Data: tlsutil.EncodeCertificatePEM(caCert)},
+		{Name: AssetPathRootCAKey, Data: tlsutil.EncodePrivateKeyPEM(caPrivKey)},
+		{Name: AssetPathRootCACert, Data: tlsutil.EncodeCertificatePEM(caCert)},
+		{Name: AssetPathKubeCAKey, Data: tlsutil.EncodePrivateKeyPEM(caPrivKey)},
+		{Name: AssetPathKubeCACert, Data: tlsutil.EncodeCertificatePEM(caCert)},
 		{Name: AssetPathAPIServerKey, Data: tlsutil.EncodePrivateKeyPEM(apiKey)},
 		{Name: AssetPathAPIServerCert, Data: tlsutil.EncodeCertificatePEM(apiCert)},
 		{Name: AssetPathServiceAccountPrivKey, Data: tlsutil.EncodePrivateKeyPEM(saPrivKey)},
